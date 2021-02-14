e0b59cfcf3c644f4249b494518e3f532eee2825c
unknown <saifalam763155@gmail.com>
2021-02-14 22:11:19 +0530

    vvv

--- js/form_details.js
+++ js/form_details.js
@@ -4,21 +4,21 @@ function addDetails(e){
     e.preventDefault();
     const ref_id=document.getElementById("ref_id").value;
     const p_details=document.getElementById("p_details").value;
-    const d_details=document.getElementById("d_details").value;
-    const t_details=document.getElementById("t_details").value;
-    const age_details=document.getElementById("age_details").value;
-    const date_value=document.getElementById("date_value").value;
-    if(!ref_id||!p_details||!d_details||!t_details||!age_details||!date_value){
+    const ph_details=document.getElementById("ph_details").value;
+    const sex_value=document.getElementById("sex_value").value;
+    // const d_details=document.getElementById("d_details").value;
+    // const t_details=document.getElementById("t_details").value;
+    // const age_details=document.getElementById("age_details").value;
+    // const date_value=document.getElementById("date_value").value;
+    if(!ref_id||!p_details||!ph_details||!sex_value||!d_details||!t_details||!age_details||!date_value){
         alert("please provide all fields data");
         return;
     }
     tableEl.innerHTML += `<tr>
                           <td>${ref_id}</td>
                           <td>${p_details}</td>
-                          <td>${d_details}</td>
-                          <td>${t_details}</td>
-                          <td>${age_details}</td>
-                          <td>${date_value}</td>
+                          <td>${ph_details}</td>
+                          <td>${sex_value}</td>
                         </tr>`  
 };
 
